a33f12cf78d70cb8134306adf7606b3ec57e9f5d
teffcode <tefy.1@live.com>
2018-10-12 14:50:47 -0500

    🎉 Home Animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,13 +3,20 @@ import styled from 'styled-components';
 
 const HomeContainer = styled.div`
   align-items: center;
-  background-color: pink;
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 100%;
+  width: 100%;
+`
+
+const HomeText = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
   justify-content: center;
-  padding: 0px 15px;
-  widht: 100vw;
+  position: absolute;
+  width: auto;
 `
 
 export const Button = styled.button`
@@ -67,23 +74,88 @@ const Input = styled.input`
   }
 `;
 
+const Ball = styled.div `
+  animation-name: points, move;
+  animation-duration: .8s, 5s;
+  animation-fill-mode: forwards;
+  animation-timing-function: steps(28), linear;
+  animation-iteration-count: infinite, 1;
+
+  background-color: yellow;
+  border-radius: 50%;
+  bottom: -100px;
+  box-shadow: 0 5px 10px black, 0 3px 3px black;
+  display: inline-block;
+  height: 30px;
+  position: fixed;
+  width: 30px;
+
+  @keyframes move {
+    from {
+      bottom: 0;
+      position: absolute;
+    }
+    to {
+      bottom: 110%;
+      position: absolute;
+    }
+  }
+  
+  @keyframes points {
+    0% {
+      background-position: 0 0;
+    }
+    100% {
+      background-position: right;
+    }
+  }
+`
+
+const Animation = styled.div `
+  background-color: pink;
+  height: 100%;
+  position: absolute;
+  overflow: hidden;
+  width: 100%;
+`
+
 class Home extends Component {
+
+  renderAnimation = () => {
+    const balls = new Array(200).fill({})
+
+    return (
+      balls.map((element, index) => {
+        const style = {
+          left: Math.floor((Math.random() * (window.innerWidth - 0))) + "px",
+          animationDelay: Math.floor((Math.random() * (80000 - 0))) + "ms"
+        }
+        return(
+          <Ball key={index} style={style}></Ball>
+        );
+      })
+    );
+  }  
+
   render() {
     return (
       <HomeContainer>
-        <PrincipalTitle>Game of Drones</PrincipalTitle>
-        <InputContainer>
-          <SecondaryTitle>Enter Player's Names</SecondaryTitle>
-          <div>
-            <Label>Player 1 <span role="img" aria-label="hand">👉🏼</span></Label>
-            <Input type="text" name="name" placeholder="Player 1 name"></Input>
-          </div>
-          <div>
-            <Label>Player 2 <span role="img" aria-label="hand">👉🏼</span></Label>
-            <Input type="text" name="name" placeholder="Player 2 name"></Input>
-          </div>
-        </InputContainer>
-        <Button>Start</Button>
+        <Animation>{ this.renderAnimation() }</Animation>
+        <HomeText>
+          <PrincipalTitle>Game of Drones</PrincipalTitle>
+          <InputContainer>
+            <SecondaryTitle>Enter Player's Names</SecondaryTitle>
+            <div>
+              <Label>Player 1 <span role="img" aria-label="hand">👉🏼</span></Label>
+              <Input type="text" name="name" placeholder="Player 1 name"></Input>
+            </div>
+            <div>
+              <Label>Player 2 <span role="img" aria-label="hand">👉🏼</span></Label>
+              <Input type="text" name="name" placeholder="Player 2 name"></Input>
+            </div>
+          </InputContainer>
+          <Button>Start</Button>
+        </HomeText>
       </HomeContainer>
     );
   }
